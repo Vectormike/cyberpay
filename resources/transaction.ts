b058cf5e41bac1e5c3d449ca878cf9424769d0f6
Redeemer40 <victorjonah199@gmail.com>
2020-05-06 09:26:22 +0100

    ...

---
+++ resources/transaction.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+
+
+
+/**
+ * @class Transaction
+ *
+ * CyberPay wrapper for communicating with CyberPay's API.
+ *  {@link https://merchant.cyberpay.ng/#/}
+ *  {@link https: API Docs}
+ */
+
+class Transaction {
+
+    /**
+     * List transactions
+     * @param {TransListOptions} options
+     */
+}
+export default Transaction;
