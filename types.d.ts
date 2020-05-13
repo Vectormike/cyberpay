7e22ee57ad3057eb92a117dba1c3f45e2b14497c
Redeemer40 <victorjonah199@gmail.com>
2020-05-13 13:42:33 +0100

    update@paymentpage options

--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,52 @@
 // Transaction
-declare interface TransOptions {
-    Currency: string,
-    MerchantRef: string,
-    Amount: number,
-    Description: string,
-    CustomerId?: string,
-    CustomerEmail?: string,
-    CustomerMobile?: string,
-    IntegrationKey?: string,
-    ReturnUrl?: string,
-    ProductCode?: string,
+declare interface PaymentOptions {
+    currency: string,
+    merchantRef: string,
+    amount: number,
+    description: string,
+    customerId?: string,
+    customerName?: string,
+    customerEmail?: string,
+    customerMobile?: string,
+    integrationKey?: string,
+    returnUrl?: string,
+    productCode?: string,
+    splits: [
+        {
+
+            amount: number,
+        }
+    ]
+
+}
+
+declare interface PaymentPageOptions {
+    firstname: string,
+    lastname: string,
+    phone: string,
+    amount: number,
+    email: string,
+    page?: number | string
+}
+
+declare interface CreateSubAccountOptions {
+    businessId: number,
+    bankAccount: string,
+    subAccountName: string,
+    bankSortCode: string,
+    businessCode: string,
+    isMainAccount: boolean,
+    walletType: string,
+    walletCode: string
+}
+
+declare interface UpdateSubAccountOptions {
+    businessCode: string,
+    businessId?: number
+    bankAccount?: string,
+    subAccountName?: string,
+    bankSortCode?: string,
+    isMainAccount?: boolean,
+    walletType?: string,
+    walletCode?:string 
 }
