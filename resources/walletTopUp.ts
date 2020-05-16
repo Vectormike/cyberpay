7adefa0a6f118ce5a07da06e32ce0c630d449da8
Redeemer40 <victorjonah199@gmail.com>
2020-05-16 17:14:41 +0100

    wallet top up and funding another wallet

---
+++ resources/walletTopUp.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import * as util from '../util';
+
+
+/**
+ * @class WalletTopUp
+ *
+ * CyberPay wrapper for communicating with CyberPay's API.
+ *  {@link https://merchant.cyberpay.ng/#/}
+ *  {@link https: API Docs}
+ */
+
+class WalletTopUp {
+    static endpoint = '/wallettopup';
+
+    /**
+       * Wallet topup
+       * @param {WalletTopUpOptions} options
+       */
+    static async walletTopUp(options: WalletTopUpOptions) {
+      return util.getResponse(axios.post(`${this.endpoint}`, options));
+    }
+
+
+    /**
+       * Top up another wallet
+       * @param {TopUpAnotherWalletOptions} options
+       */
+    static async topUpAnotherWallet(options: TopUpAnotherWalletOptions) {
+      return util.getResponse(axios.post(`${this.endpoint}`, options));
+    }
+}
+
+export default WalletTopUp;
