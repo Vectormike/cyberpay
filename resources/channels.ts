163594e4873d381bb0887d4efbcfee0c229b3879
Redeemer40 <victorjonah199@gmail.com>
2020-05-17 07:55:25 +0100

    method for channels integrated

---
+++ resources/channels.ts
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-unresolved
+
+import axios from 'axios';
+import * as util from '../util';
+
+/**
+ * @class Channels
+ *
+ * CyberPay wrapper for communicating with CyberPay's API.
+ *  {@link https://payment-api.cyberpay.ng/api/v1/}
+ *  {@link https: API Docs}
+ */
+
+class Channels {
+  static endpoint = '/channels';
+
+  /**
+     * List all channels
+     *
+     */
+  static async get() {
+    return util.getResponse(axios.get(`${this.endpoint}`));
+  }
+
+
+  /**
+     * List all active channels
+     *
+     */
+  static async getActive() {
+    return util.getResponse(axios.get(`${this.endpoint}`));
+  }
+}
+
+export default Channels;
