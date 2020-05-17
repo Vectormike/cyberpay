cb18bcf48d57b4c1bfff561dd5216a51d2b63a53
Redeemer40 <victorjonah199@gmail.com>
2020-05-17 07:41:33 +0100

    bank method

---
+++ resources/bank.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-unresolved
+import axios from 'axios';
+import * as util from '../util';
+
+/**
+ * @class Bank
+ *
+ * CyberPay wrapper for communicating with CyberPay's API.
+ *  {@link https://payment-api.cyberpay.ng/api/v1/}
+ *  {@link https: API Docs}
+ */
+
+class Banks {
+  static endpoint = '/banks';
+
+  /**
+     * List of all banks integrated
+     *
+     */
+  static async get() {
+    return util.getResponse(axios.get(`${this.endpoint}`));
+  }
+}
+
+export default Banks;
